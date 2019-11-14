7e4e9d9de28873dc4578da2766b42d24e127e59a
Richard Bramley <rabramley@gmail.com>
2019-11-14 14:38:12 +0000

    styling

--- snippets/header.php
+++ snippets/header.php
@@ -17,7 +17,7 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-md navbar-light fixed-top">
+    <nav class="navbar navbar-expand-md fixed-top">
       <div class="container">
         <a class="navbar-brand" href="/">
           <img src="/pages/vicori_logo_150.png" />
