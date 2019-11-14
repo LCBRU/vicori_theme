f5e7a96a4d5e30682ed5e3e7eafea1653e59776d
Richard Bramley <rabramley@gmail.com>
2019-11-14 13:01:39 +0000

    styling

--- snippets/header.php
+++ snippets/header.php
@@ -27,37 +27,6 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-  
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="/">
-            <img src="/pages/vicori_logo_150.png" />
-          </a>
-        </div>
-
-        <div>
-          <ul class="nav navbar-nav">
             <@ newPagelist {
               type: 'children', 
               parent: '/' 
@@ -68,7 +37,7 @@
               </li>
             <@ end @>
           </li>
-        </div>
+          </div>
       </div>
     </nav>
 
