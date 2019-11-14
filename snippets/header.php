afbbb12d476cf5dc52f6e9b3e91fbb401d93d553
Richard Bramley <rabramley@gmail.com>
2019-11-14 14:02:20 +0000

    styling

--- snippets/header.php
+++ snippets/header.php
@@ -24,7 +24,7 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="collapse navbar-toggleable-sm" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <@ newPagelist {
             type: 'children', 
